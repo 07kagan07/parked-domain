5ca7e097cf3cc81629e3fdbb4315364e368f97b3
07kagan07 <kagancoskun32@gmail.com>
2023-09-28 15:22:09 +0300

    btn disabled

--- src/app/components/Park.js
+++ src/app/components/Park.js
@@ -87,7 +87,8 @@ const Park = () => {
             </div>
           </div>
           <button
-            disabled={values.includes("")}
+            // disabled={values.includes("")}
+            disabled
             className="px-5 py-2 border-2 rounded-lg border-[#82b37a] hover:bg-[#82b37a] hover:text-white disabled:bg-[#686A6C] disabled:text-[#b3b7bb] disabled:border-[#686A6C]"
             type="submit"
           >
